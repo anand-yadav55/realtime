116d13baa73dbd7404dce902d1957482eb7e4871
anand-yadav55 <anand55yadav@gmail.com>
2020-10-15 10:19:24 +0530

    for local host

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) =>{
 })
 
 //  Mongoose Setup =======================================================
-mongoose.connect('mongodb+srv://anand:Anandyadav@1@realtimeapp.yntiq.mongodb.net/RealtimeApp?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/MyDatabase', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=>{
     console.log("1.db conneted");
   })
